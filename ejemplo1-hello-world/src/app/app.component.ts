b57dd5475a63112344691a803d1c06d057024676
anaisabelveg <anaisabelveg@gmail.com>
2022-09-13 20:01:31 +0200

    Add files via upload

--- ejemplo1-hello-world/src/app/app.component.ts
+++ ejemplo1-hello-world/src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre: string = 'Anabel';
+  apellido: string = 'Vegas';
+  edad: number = 52;
+
+  deshabilitado: boolean = true;
+
+  estilo: string = '';
+
+  constructor(){
+    // Crear un temporizador que trancurridos 3 segundos
+    // se habilite el boton
+    //setTimeout(que_hacemos, tiempo)
+    setTimeout(() => {
+      this.deshabilitado = false;
+    } , 3000);
+  }
+
+  saludar(): void{
+    alert('Hola');
+  }
 }
